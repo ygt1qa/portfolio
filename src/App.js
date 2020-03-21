32f3f9a3877622bba4523a2e0a3f989cd87feb6c
YASUYUKI <ygt1qa3@gmail.com>
2020-03-21 16:48:21 +0900

    add ticket

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import './styles/styles.scss';
 import './vendor/pictonic/css/pictonic.scss';
 import Content from './components/Content';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Ticket from './components/Ticket';
 
 function App() {
   return (
@@ -11,6 +19,7 @@ function App() {
       <Header />
       <Navigation />
       <Content />
+      <Ticket />
     </div>
   );
 }
---
+++ src/components/Ticket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Ticket() {
+  return (
+    <div className="ticket">
+      Welcome My Portfolio!!
+    </div>
+  )
+}
+
+export default Ticket;
